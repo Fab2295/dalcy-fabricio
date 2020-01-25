373fce24622cb268c5de058baad803007d469db6
Fabrício Medeiros <fabricio221995@gmail.com>
2020-01-24 22:02:28 -0300

    Change About.js

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -3,29 +3,21 @@ import ScrollAnimation from "react-animate-on-scroll";
 
 import "./about.css";
 
-export default class About extends Component {
-  render() {
-    return (
-      <div className="about">
-        <div className="container h-100">
-          <div className="row pt-5 h-25 align-items-center">
-            <div className="col-sm-12">
-              <h1 className="text-center font-h1">Find out more about me</h1>
-            </div>
+export default function About() {
+  return (
+    <div className="about">
+      <div className="container h-100">
+        <div className="row h-100 align-items-center">
+          <div className="col-sm-4">
+            <h1 className="text-center title">About me</h1>
           </div>
-          <div className="row h-75 align-items-center text-center">
-            <div className="col-sm">
-              <ScrollAnimation animateIn="bounceInLeft">
-                <h2>Bio</h2>
-                <p id="text">Hello! I am developer.</p>
-              </ScrollAnimation>
-            </div>
-            <div className="col-sm">
-              <h1>Fotos</h1>
-            </div>
+          <div className="col-sm-8">
+            <ScrollAnimation animateIn="fadeIn">
+              <p className="text-center">Hello! I am developer.</p>
+            </ScrollAnimation>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
